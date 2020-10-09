81b6f4ea03f280539e986ce99224490b46f76d56
tgp4tech <thiagogodinhopaschoalin@live.com>
2020-10-09 01:29:18 -0300

    Factory: fix Result;

--- src/Tgp.FluentResult/Factories/Result.cs
+++ src/Tgp.FluentResult/Factories/Result.cs
@@ -22,45 +22,33 @@ namespace Tgp.FluentResult
 
 
         /// <summary>
+        /// Retorno de aviso com uma mensagem de aviso para o usuário
+        /// </summary>
+        /// <param name="message">Mensagem descritiva sobre o aviso</param>
+        /// <param name="statusCode"><see cref="HttpStatusCode"/></param>
+        /// <returns><see cref="Result"/></returns>
+        public static Result Warning(string message, HttpStatusCode statusCode = HttpStatusCode.Conflict)
+            => new Result(new MetaWarn(message), statusCode);
+
+
+        /// <summary>
         /// Retorno de sucesso sem nenhuma mensagem para o usuário
         /// </summary>
         /// <param name="statusCode"><see cref="HttpStatusCode"/></param>
         /// <returns><see cref="Result"/></returns>
         public static Result Success(HttpStatusCode statusCode = HttpStatusCode.OK)
-            => new Result(new MetaHit(null), statusCode);
+            => new Result(new MetaHit(), statusCode);
 
         /// <summary>
-        /// Retorno de sucesso com uma mensagem para o usuário
-        /// </summary>
-        /// <param name="message">Mensagem amigável para o usuário</param>
-        /// <param name="statusCode"><see cref="HttpStatusCode"/></param>
-        /// <returns><see cref="Result"/></returns>
-        public static Result Success(string message, HttpStatusCode statusCode = HttpStatusCode.OK)
-            => new Result(new MetaHit(message), statusCode);
-
-
-        /// <summary>
-        /// Retorno de sucesso com um objeto de dados para o usuário
+        ///  Retorno de sucesso com um objeto de dados para o usuário
         /// </summary>
         /// <typeparam name="TResponse">Objeto de valor complexo</typeparam>
         /// <param name="response">Entidade de resposta</param>
-        /// <returns><see cref="Result{TResponse}"/></returns>
-        public static Result<TResponse> Success<TResponse>(TResponse response)
-            where TResponse : class
-            => Result<TResponse>.Success(response);
-
-        /// <summary>
-        /// Retorno de sucesso com um objeto de dados e uma mensagem para o usuário
-        /// </summary>
-        /// <typeparam name="TResponse">Objeto de valor complexo</typeparam>
-        /// <param name="response">Entidade de resposta</param>
-        /// <param name="message">Mensagem amigável para o usuário</param>
         /// <param name="statusCode"><see cref="HttpStatusCode"/></param>
         /// <returns><see cref="Result{TResponse}"/></returns>
-        public static Result<TResponse> Success<TResponse>(TResponse response, string message, HttpStatusCode statusCode = HttpStatusCode.OK)
+        public static Result<TResponse> Success<TResponse>(TResponse response, HttpStatusCode statusCode = HttpStatusCode.OK)
             where TResponse : class
-            => Result<TResponse>.Success(response, message, statusCode);
-
+            => Result<TResponse>.Success(response, statusCode);
 
         /// <summary>
         /// Abstração de execução de uma consulta
@@ -99,22 +87,14 @@ namespace Tgp.FluentResult
     public partial class Result<TResponse>
     {
         /// <summary>
-        /// Retorno de sucesso com um objeto de dados para o usuário
-        /// </summary>
-        /// <param name="response">Entidade de resposta</param>
-        /// <returns><see cref="Result{TResponse}"/></returns>
-        public static Result<TResponse> Success(TResponse response)
-            => new Result<TResponse>(response, new MetaHit(null), HttpStatusCode.OK);
-
-        /// <summary>
         /// Retorno de sucesso com um objeto de dados e uma mensagem para o usuário
         /// </summary>
         /// <param name="response">Entidade de resposta</param>
         /// <param name="message">Mensagem amigável para o usuário</param>
         /// <param name="statusCode"><see cref="HttpStatusCode"/></param>
         /// <returns><see cref="Result{TResponse}"/></returns>
-        public static Result<TResponse> Success(TResponse response, string message, HttpStatusCode statusCode = HttpStatusCode.OK)
-            => new Result<TResponse>(response, new MetaHit(message), statusCode);
+        public static Result<TResponse> Success(TResponse response, HttpStatusCode statusCode = HttpStatusCode.OK)
+            => new Result<TResponse>(response, new MetaHit(), statusCode);
 
         /// <summary>
         /// Abstração de execução de uma consulta
@@ -133,16 +113,17 @@ namespace Tgp.FluentResult
 
                 if (result is null || result is default(TResponse) || (result is IEnumerable enumerable && !enumerable.GetEnumerator().MoveNext()))
                 {
-                    return new Result<TResponse>(result, new MetaHit("Nada foi encontrado."), HttpStatusCode.NotFound);
+                    return new Result<TResponse>(result, new MetaWarn("Retorno sem resultado."), HttpStatusCode.NotFound);
                 }
 
-                return new Result<TResponse>(result, new MetaHit("Dados recuperados com sucesso."), HttpStatusCode.OK);
+                return new Result<TResponse>(result, new MetaHit(), HttpStatusCode.OK);
             }
             catch (Exception ex)
             {
                 return new Result(new MetaError(errorMsg, ex).AddChunk("CallerMemberName", @callerMethodName), HttpStatusCode.BadRequest);
             }
         }
+
 
         /// <summary>
         /// Result completo. Monte o resultado que for mais conveniente
