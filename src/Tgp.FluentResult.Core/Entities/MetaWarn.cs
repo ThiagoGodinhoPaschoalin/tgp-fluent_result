ca11ad7de536a7304da9bf4ef6359248c713b1fa
tgp4tech <thiagogodinhopaschoalin@live.com>
2020-10-09 00:54:31 -0300

    Core: Add MetaWarn;

---
+++ src/Tgp.FluentResult.Core/Entities/MetaWarn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Tgp.FluentResult.Core.Interfaces;
+using Tgp.FluentResult.Core.Models;
+
+namespace Tgp.FluentResult.Core.Entities
+{
+    /// <summary>
+    /// Metadado padrão para Aviso
+    /// </summary>
+    public sealed class MetaWarn : Metadata, IMetaWarn
+    {
+        /// <summary>
+        /// Mensagem descritiva do Metadado de Aviso
+        /// </summary>
+        public string Message { get; private set; }
+
+        /// <summary>
+        /// Metadado para Aviso
+        /// </summary>
+        /// <param name="message">Mensagem</param>
+        public MetaWarn(string message)
+            : base()
+        {
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                throw new ArgumentNullException(nameof(message), "Property is Mandatory!");
+            }
+
+            this.Message = message;
+        }
+    }
+}
---
+++ src/Tgp.FluentResult.Core/Interfaces/IMetaWarn.cs
@@ -0,0 +1,13 @@
+﻿namespace Tgp.FluentResult.Core.Interfaces
+{
+    /// <summary>
+    /// Interface de Metadado para Aviso
+    /// </summary>
+    public interface IMetaWarn : IMetadata
+    {
+        /// <summary>
+        /// Mensagem descritiva do Metadado do Aviso
+        /// </summary>
+        string Message { get; }
+    }
+}
